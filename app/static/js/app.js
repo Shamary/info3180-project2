0ccef465bb8f6e26f3cf9bee50724a072e98bea0
shamary <shamarywilliams@gmail.com>
2017-04-04 04:58:12 +0000

    ex 3

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,8 +3,8 @@ var app=angular.module("thumbnail",[]);
 app.controller("myCtlr",function($scope,$http){
     $http.post("http://info3180-lab7-shamary.c9users.io:8080/api/thumbnails","")
     .then(function(result){
-        $scope.img_lst=result.data['thumbnails'];
+        $scope.img_lst=result.data.thumbnails;
         
-        alert(result.data['thumbnails']);
+        //alert(result.data.thumbnails);
     });
 });
